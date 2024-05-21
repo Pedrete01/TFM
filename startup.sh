e07616b7502bd26b01b9fa79a63deb72cab4ab3d
Pedrete01 <76138730+Pedrete01@users.noreply.github.com>
2024-05-21 11:35:37 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Instalar dependencias del sistema
 apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0
-
-# Ejecutar el comando original para iniciar la aplicación
 gunicorn server:app
